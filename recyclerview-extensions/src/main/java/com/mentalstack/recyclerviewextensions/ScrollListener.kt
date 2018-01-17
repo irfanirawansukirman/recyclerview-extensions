bb0a0a35bdcb1b00b908e15a2505222994e0f748
denis aleksandrov <guitariz@mail.ru>
2018-01-17 08:45:51 +0300

    add from previous

--- recyclerview-extensions/src/main/java/com/mentalstack/recyclerviewextensions/ScrollListener.kt
+++ recyclerview-extensions/src/main/java/com/mentalstack/recyclerviewextensions/ScrollListener.kt
@@ -6,32 +6,54 @@ import android.widget.LinearLayout
 /**
  * Created by aleksandrovdenis on 15.01.2018.
  */
-internal class ScrollListener(
-        private val adapter: RecyclerAdapter) : RecyclerView.OnScrollListener() {
+
+internal enum class PagieDirection { START, END }
+
+internal class ScrollListener(private val adapter: RecyclerAdapter) : RecyclerView.OnScrollListener() {
+
+    private var dX = 0
+    private var dY = 0
+
+    private var orientation: Int? = null
+
+    override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+        super.onScrollStateChanged(recyclerView, newState)
+        if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+            recyclerView?.let { recycler ->
+                if (orientation == null)
+                    orientation = recycler.getOrientation()
+
+                orientation?.let { orientation ->
+                    calcPaginationDirection(recycler, orientation)?.let { direction ->
+                        adapter.processPagination(direction)
+                    }
+                }
+            }
+            dX = 0
+            dY = 0
+        }
+    }
 
     override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+        dX += dx
+        dY += dy
         super.onScrolled(recyclerView, dx, dy)
-        recyclerView?.let { recycler ->
-            recycler.getDirection()?.let { direction ->
-                adapter.processPagination(calcPaginationDirection(recycler, direction, dx, dy))
-            }
-        }
     }
 
-    private fun calcPaginationDirection(recycler: RecyclerView, dir: Int, dx: Int, dy: Int): PagieDirection {
-        return when (dir) {
-            LinearLayout.HORIZONTAL -> when {
-                dx < 0 && recycler.firstVibleIndex() == 0 -> PagieDirection.START
-                dx > 0 && recycler.lastVisibleIndex() == adapter.itemCount -> PagieDirection.END
-                else -> PagieDirection.NONE
+    private fun calcPaginationDirection(recycler: RecyclerView, dir: Int) =
+            when (dir) {
+                LinearLayout.HORIZONTAL -> dX
+                LinearLayout.VERTICAL -> dY
+                else -> null
+            }?.let { delta ->
+                when {
+                    delta < 0 && recycler.firstVibleIndex()?.let
+                    { it < adapter.paginationSensitive } ?: false ->
+                        PagieDirection.START
+                    delta > 0 && recycler.lastVisibleIndex()?.let
+                    { it > adapter.itemCount - adapter.paginationSensitive } ?: false ->
+                        PagieDirection.END
+                    else -> null
+                }
             }
-            LinearLayout.VERTICAL -> when {
-                dy < 0 && recycler.firstVibleIndex() == 0 -> PagieDirection.START
-                dy > 0 && recycler.lastVisibleIndex() == adapter.itemCount -> PagieDirection.END
-                else -> PagieDirection.NONE
-            }
-            else -> PagieDirection.NONE
-        }
-    }
-
 }
